6175d04e50a492941c640a21a62fd9b51485d74a
Semin <Aleksandr.Semin@harman.com>
2019-12-18 17:10:50 +0300

    Implemented BnB

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import numpy as np
 from os import listdir
 from os.path import join
+from itertools import product
 
 
 def read_instance(filename):
     with open(filename, "r") as f:
-        times = f.readline().split()
+        times = [0] + [int(v) for v in f.readline().split()]
         costs = list()
-        for i in range(len(times)):
+        for i in range(len(times)+1):
             costs.append([int(v) for v in f.readline().split()])
         opt_path = [int(v) for v in f.readline().split()]
         opt_value = [int(v) for v in f.readline().split()]
@@ -22,14 +23,14 @@ def save_results(filename, path, value):
 
 
 class Task:
-    def __init__(self, costs, times, opt_path=None, opt_value=None):
+    def __init__(self, times, costs, opt_path=None, opt_value=None):
         self.costs = costs
         self.times = times
         self.opt_path = opt_path
         self.opt_value = opt_value
 
         self.n = len(times)
-        self.full = set(range(self.n))
+        self.full = set(range(1, self.n))
 
     def distance(self, path):
         dist = 0
@@ -38,6 +39,13 @@ class Task:
             dist += self.costs[prev][v]
             prev = v
         return dist
+
+    def cost_function(self, path):
+        w = 0
+        for i in range(1, len(path) + 1):
+            if self.distance(path[:i]) > self.times[path[i-1]]:
+                w += 1
+        return w
 
 
 class Vertex:
@@ -48,6 +56,12 @@ class Vertex:
         if other not in self.path:
             return Vertex(self.path + [other])
         return Vertex(self.path)
+
+    def __str__(self):
+        return "Vertex: Path = {}".format(self.path)
+
+    def __repr__(self):
+        return self.__str__()
 
     @property
     def path_set(self):
@@ -65,28 +79,29 @@ def argmin(now, destines, z, task, ):
 
 
 def upper_bound(vertex, task):
-    beta = list(task.full - vertex.path_set)
-
-    if len(beta) != 0:
+    xkj = None
+    for _ in range(task.n - len(vertex.path) - 1):
+        beta = list(task.full - vertex.path_set)
         z = task.distance(vertex.path)
-        h = argmin(vertex.path[-1], beta, z, task)
+        last_vertex = vertex.path[-1]
+        h = argmin(last_vertex, beta, z, task)
         xkj = beta[h]
-        if len(beta) > 1:
-            return upper_bound(vertex + xkj, task)
-        else:
-            return z + task.costs[vertex.path[-1]][xkj]
+        vertex = vertex + xkj
+    if xkj:
+        return task.cost_function(vertex.path+[xkj])
     else:
-        return task.distance(vertex.path)
+        return task.cost_function(vertex.path)
 
 
 def lower_bound(vertex, task):
     beta = list(task.full - vertex.path_set)
-    za = [task.distance(vertex.path[:i]) for i in range(1, len(vertex.path))]
+    z = { vertex.path[i-1]: task.distance(vertex.path[:i]) for i in range(1, len(vertex.path) + 1)}
+
     vert_cost = task.distance(vertex.path)
     last_vert = vertex.path[-1]
-    zb = [vert_cost + task.costs[last_vert][i] for i in beta]
-    z = za + zb
-    return np.sum([0 if z[i] < task.times[i] else 1 for i in range(task.n)])
+    zb = {i: vert_cost + task.costs[last_vert][i] for i in beta}
+    z.update(zb)
+    return np.sum([0 if z[i] < task.times[i] else 1 for i in range(1, task.n)])
 
 
 def brunching_wide(leafs):
@@ -102,16 +117,74 @@ def brunching_deep(leafs):
 
 
 def brunch_and_bound(task, brunching, upper_calc, lower_calc):
+    # step 1
+    candidats = set()
+    V = set([Vertex([i]) for i in range(1, task.n)])
+
+    while True:
+        # step 3
+        v = brunching(V)
+        beta_all = task.full - v.path_set
+
+        # step 3
+        V.remove(v)
+        if len(v.path) == task.n - 1:
+            candidats.add(v)
+            cost = task.cost_function(v.path)
+            dist = task.distance(v.path)
+            print("\t\t", v, " COST = ", cost, "  DIST = ", dist)
+        for beta in beta_all:
+            V.add(v + beta)
+
+        # step 4
+        remove = set()
+        for v1, v2 in product(V, V):
+            if v1 == v2:
+                continue
+            # print("BbB :: Check 1 :: ", v1, v2)
+            # print("BbB :: Check 2 :: ", upper_calc(v1, task), lower_calc(v2, task))
+            if v1 in V and v2 in V:
+                if upper_calc(v1, task) <= lower_calc(v2, task):
+                    # remove.add(v2)
+                    V.remove(v2)
+        print("BnB Iteration : FOR REMOVE", remove)
+        for v in remove:
+            V.remove(v)
+
+        # step 2
+        print("BnB Iteration : ", V, "\n\t\t SIZE = ", len(V))
+
+        if len(V) == 0:
+            print("BnB: Last step", candidats)
+            cost = task.cost_function(v.path)
+            dist = task.distance(v.path)
+            print("\t\t", v, " COST = ", cost, "  DIST = ", dist)
+            # for cand in candidats:
+            #     cost = task.cost_function(cand.path)
+            #     dist = task.distance(cand.path)
+            #     print("\t\t", cand, " COST = ", cost, "  DIST = ", dist)
+
+
+            # v = list(V)[-1]
+            # lower = lower_calc(v, task)
+            # upper = upper_calc(v, task)
+            # if lower == upper:
+            #     return v.path, None
+            break
     return None, None
 
 
 def main():
     instances_folder = "instances"
     for file in listdir(instances_folder):
-        if file[-4:] != ".txt":
+        if file[-5:] != "3.txt":
             continue
         else:
             task = Task(*read_instance(join(instances_folder, file)))
             founded_path, founded_value = brunch_and_bound(task, brunching_deep, upper_bound, lower_bound)
-            if founded_path == task.opt_path and founded_value == task.opt_value:
+            if founded_path == task.opt_path:  # and founded_value == task.opt_value:
                 save_results(file, founded_path, founded_value)
+
+
+if __name__ == "__main__":
+    main()
